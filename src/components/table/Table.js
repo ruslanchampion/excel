1ce3f9678b5fe734d52749c2b82af2343f594dad
ruslanchampion <ruslan.timbakov@gmail.com>
2021-01-13 12:33:14 +0800

    feat: add resize functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
 import {ExcelComponents} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions'
 
 export class Table extends ExcelComponents {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
+  }
+
   toHTML() {
     return createTable()
   }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
+    }
+  }
 }
+
